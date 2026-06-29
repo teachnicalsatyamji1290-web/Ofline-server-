b6fd0f37598a8e339f1a1604f91357c9a81ccefd
teachnicalsatyamji1290-web <teachnicalsatyamji1290@gmail.com>
2026-06-29 18:05:55 +0530

    Change server port from 20985 to 22907

--- bot.py
+++ bot.py
@@ -363,4 +363,4 @@ def get_logs():
     return jsonify({'logs': logs[-50:]})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=20985, debug=False)
+    app.run(host='0.0.0.0', port=22907, debug=False)
